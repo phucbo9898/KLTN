2e280954b11365368d980bb16cf2205f9e97a49a
phucbo9898 <86301310+phucbo9898@users.noreply.github.com>
2023-05-18 20:49:29 +0700

    Update CMS + FE

--- resources/views/cms/login/login.blade.php
+++ resources/views/cms/login/login.blade.php
@@ -16,70 +16,66 @@
 </head>
 
 <body class="bg-gradient-primary">
-    <div class="container">
-        <!-- Outer Row -->
-        <div class="row justify-content-center">
-            <div class="col-xl-10 col-lg-12 col-md-9">
-                <div class="card o-hidden border-0 shadow-lg my-5">
-                    <div class="card-body p-0">
-                        <!-- Nested Row within Card Body -->
-                        <div class="row">
-                            <div class="col-lg-6 d-none d-lg-block" style="overflow: hidden">
-                                <img style="width:464,98px; height:500px" src="{{ asset('login/login-admin.jpg') }}" />
-                            </div>
-                            <div class="col-lg-6">
-                                <div class="p-5">
-                                    <div class="text-center">
-                                        <h1 class="h4 text-gray-900 mb-5">Đăng nhập Admin</h1>
-                                    </div>
-                                    @if (\Session::has('error'))
-                                        <div class="alert alert-success alert-dismissible fade show" role="alert">
-                                            <strong>Thất bại! </strong> {{ \Session::get('error') }}.
-                                            <button type="button" class="close" data-dismiss="alert"
-                                                aria-label="Close">
-                                                <span aria-hidden="true">&times;</span>
-                                            </button>
-                                        </div>
-                                    @endif
-                                    <form action="{{ route('admin.login') }}" method="POST">
-                                        @csrf
-                                        <div class="form-group">
-                                            <input type="email" name="email" class="form-control form-control-user"
-                                                id="exampleInputEmail" aria-describedby="emailHelp"
-                                                placeholder="Enter Email Address...">
-                                        </div>
-                                        <div class="form-group">
-                                            <input type="password" name="password"
-                                                class="form-control form-control-user" id="exampleInputPassword"
-                                                placeholder="Password">
-                                        </div>
-                                        <div class="form-group">
-                                            <input type="submit" class="btn btn-primary btn-user btn-block mt-2"
-                                                value="Đăng nhập" />
-                                        </div>
-                                    </form>
-                                    <hr>
+<div class="container">
+    <!-- Outer Row -->
+    <div class="row justify-content-center">
+        <div class="col-xl-10 col-lg-12 col-md-9">
+            <div class="card o-hidden border-0 shadow-lg my-5">
+                <div class="card-body p-0">
+                    <!-- Nested Row within Card Body -->
+                    <div class="row">
+                        <div class="col-lg-6 d-none d-lg-block" style="overflow: hidden">
+                            <img style="width:464.98px; height:500px" src="{{ asset('login/login-admin.jpg') }}"/>
+                        </div>
+                        <div class="col-lg-6">
+                            <div class="p-5">
+                                <div class="text-center">
+                                    <h1 class="h4 text-gray-900 mb-5">Đăng nhập Admin</h1>
                                 </div>
+                                @if (\Session::has('error'))
+                                    <div class="alert alert-success alert-dismissible fade show" role="alert">
+                                        <strong>Thất bại! </strong> {{ \Session::get('error') }}.
+                                        <button type="button" class="close" data-dismiss="alert"
+                                                aria-label="Close">
+                                            <span aria-hidden="true">&times;</span>
+                                        </button>
+                                    </div>
+                                @endif
+                                <form action="{{ route('admin.login') }}" method="POST">
+                                    @csrf
+                                    <div class="form-group">
+                                        <input type="email" name="email" class="form-control form-control-user"
+                                               id="exampleInputEmail" aria-describedby="emailHelp"
+                                               placeholder="Nhập email">
+                                    </div>
+                                    <div class="form-group">
+                                        <input type="password" name="password"
+                                               class="form-control form-control-user" id="exampleInputPassword"
+                                               placeholder="Nhập mật khẩu">
+                                    </div>
+                                    <div class="form-group">
+                                        <input type="submit" class="btn btn-primary btn-user btn-block mt-2"
+                                               value="Đăng nhập"/>
+                                    </div>
+                                </form><hr>
                             </div>
                         </div>
                     </div>
                 </div>
-
             </div>
-
         </div>
-
     </div>
+</div>
 
-    <!-- Bootstrap core JavaScript-->
-    <script src="{{ asset('login/jquery.min.js') }}"></script>
-    <script src="{{ asset('login/bootstrap.bundle.min.js') }}"></script>
+<!-- Bootstrap core JavaScript-->
+<script src="{{ asset('login/jquery.min.js') }}"></script>
+<script src="{{ asset('login/bootstrap.bundle.min.js') }}"></script>
 
-    <!-- Core plugin JavaScript-->
-    <script src="{{ asset('login/jquery.easing.min.js') }}"></script>
+<!-- Core plugin JavaScript-->
+<script src="{{ asset('login/jquery.easing.min.js') }}"></script>
 
-    <!-- Custom scripts for all pages-->
-    <script src="{{ asset('login/sb-admin-2.min.js') }}"></script>
+<!-- Custom scripts for all pages-->
+<script src="{{ asset('login/sb-admin-2.min.js') }}"></script>
 
 </body>
 
